b000e143cf13c69edabe7d39100394c1d2f76bc6
Shairahavan Selvachandran <shairahavanselvachandran@Shairahavans-Laptop.local>
2025-09-06 23:51:49 -0400

    database change to hopefully make it faster

--- backend/beesweeper/settings.py
+++ backend/beesweeper/settings.py
@@ -4,6 +4,8 @@ Django settings for beesweeper project.
 
 import os
 from pathlib import Path
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -69,11 +71,9 @@ WSGI_APPLICATION = "beesweeper.wsgi.application"
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
+
 # If you want PostgreSQL on Render later, replace with:
 # DATABASES = {
 #     "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
